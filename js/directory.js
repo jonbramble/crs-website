68498ac17381ad5e55450d03ad01db15ed3387a1
Jonathan Bramble <j.bramble@sheffield.ac.uk>
2014-07-30 19:02:12 +0100

    added search tool

--- js/directory.js
+++ js/directory.js
@@ -1,7 +1,40 @@
+search = function(text){
+  $('#search-results').html("<div></div>");
+  var arr = $.grep(list, function(e){ return e.keyword == text;});
+   $.each(arr, function(index,element){
+
+   	var row = $("<div></div>").addClass("row");
+
+    var name = element.member;
+    var id = element.id;
+    var keyword = element.keyword;
+    var link = "<a href='#' data-reveal-id='"+id+"'>"+name+"</a>";
+    var keyword_column = $("<div></div>").addClass("large-6 columns").append(keyword);
+   	var member_column = $("<div></div>").addClass("large-6 columns").append(link);
+   	row.append(keyword_column);
+   	row.append(member_column);
+
+   	$('#search-results').append(row);
+  }); 
+   $("#search-results-row").fadeIn("slow");
+}
+
+
 $(document).ready(function(){
- $(".directory-items li span").each(function(index,element){
- 	
-  console.log($(element).data());
- });
- 
+	//retrieve data in an array from the directory listings
+	list = Array();
+ 	$(".directory-items li span").each(function(index,element){
+ 		var member = $(element).data().member;
+ 		var id = $(element).data().id;
+ 		var keyword = $(element).data().keyword; //should this be forced lowercase
+ 		list.push({member: member, id: id, keyword: keyword});
+ 	});
+
+   $("#search-results-row").hide();
+
+   //add a click event to the search keywords box
+   $("#search-keywords").on('click', function(){
+   		var text = $("#keywords-text").val();
+   		search(text);
+   	});
 });
